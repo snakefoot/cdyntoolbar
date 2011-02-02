6edac28cf82d48fd272e3b7b113a4974bd3531c7
sweaty1 <sweaty1@6bb060f5-b3e4-d176-1c5f-5f5a4e21b952>
2011-02-02 22:54:39 +0000

    Add buttons dynamically instead of from static resources

--- trunk/CDynToolbar/DynToolBar.cpp
+++ trunk/CDynToolbar/DynToolBar.cpp
@@ -34,6 +34,200 @@ BEGIN_MESSAGE_MAP(CDynToolBar, CToolBar)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
+bool CDynToolBar::AddSeparator()
+{
+	return AddButton(0, (HICON)NULL);
+}
+
+bool CDynToolBar::AddButton(int nIdCommand, HICON hIcon)
+{
+	TBBUTTON tbb = {0};
+	tbb.idCommand = nIdCommand;
+	tbb.iString = -1;	// Text drawn next to the button
+	tbb.fsState = TBSTATE_ENABLED;
+	tbb.fsStyle = nIdCommand==0 ? TBSTYLE_SEP : TBSTYLE_BUTTON;
+	return AddButton(tbb, hIcon);
+}
+
+bool CDynToolBar::AddButton(int nIdCommand, UINT nIDResource)
+{
+	//load the icon from the resources
+	if (nIDResource != NULL)
+	{
+		HINSTANCE hInst = 
+			AfxFindResourceHandle(MAKEINTRESOURCE(nIDResource),RT_GROUP_ICON);
+		HICON hIcon = (HICON)LoadImage(hInst,MAKEINTRESOURCE(nIDResource),
+			IMAGE_ICON,32,32,LR_DEFAULTCOLOR);
+		return AddButton(nIdCommand, hIcon);
+	}
+	else
+	{
+		return AddButton(nIdCommand, (HICON)NULL);
+	}
+}
+
+bool CDynToolBar::AddButton(int nIdCommand, LPCTSTR lpszResourceName)
+{
+	//load the icon from the resources
+	if (lpszResourceName != NULL)
+	{
+		HINSTANCE hInst = 
+			AfxFindResourceHandle(MAKEINTRESOURCE(lpszResourceName),RT_GROUP_ICON);
+		HICON hIcon = (HICON)LoadImage(hInst,MAKEINTRESOURCE(lpszResourceName),
+			IMAGE_ICON,32,32,LR_DEFAULTCOLOR);
+		return AddButton(nIdCommand, hIcon);
+	}
+	else
+	{
+		return AddButton(nIdCommand, (HICON)NULL);
+	}
+}
+
+namespace
+{
+	HICON CreateGrayscaleIcon( HICON hIcon, COLORREF* pPalette )
+	{
+		if (hIcon == NULL)
+		{
+			return NULL;
+		}
+	 
+		HDC hdc = ::GetDC(NULL);
+	 
+		HICON      hGrayIcon      = NULL;
+		ICONINFO   icInfo         = { 0 };
+		ICONINFO   icGrayInfo     = { 0 };
+		LPDWORD    lpBits         = NULL;
+		LPBYTE     lpBitsPtr      = NULL;
+		SIZE sz;
+		DWORD c1 = 0;
+		BITMAPINFO bmpInfo        = { 0 };
+		bmpInfo.bmiHeader.biSize  = sizeof(BITMAPINFOHEADER);
+	 
+		if (::GetIconInfo(hIcon, &icInfo))
+		{
+			if (::GetDIBits(hdc, icInfo.hbmColor, 0, 0, NULL, &bmpInfo, DIB_RGB_COLORS) != 0)
+			{
+				bmpInfo.bmiHeader.biCompression = BI_RGB;
+				bmpInfo.bmiHeader.biPlanes = 1;
+				bmpInfo.bmiHeader.biBitCount = 32;
+				bmpInfo.bmiHeader.biSizeImage = bmpInfo.bmiHeader.biWidth * 4 * bmpInfo.bmiHeader.biHeight;
+				bmpInfo.bmiHeader.biClrUsed = 0;
+				bmpInfo.bmiHeader.biClrImportant = 0;
+	 
+				const int size = sizeof(BITMAPINFOHEADER) + sizeof(RGBQUAD) * bmpInfo.bmiHeader.biClrUsed;
+				BITMAPINFO* pBmpInfo = (BITMAPINFO*)new BYTE[size];
+				memset (pBmpInfo, 0, size);
+				pBmpInfo->bmiHeader = bmpInfo.bmiHeader;
+				pBmpInfo->bmiHeader.biCompression = BI_RGB;
+	 
+				sz.cx = bmpInfo.bmiHeader.biWidth;
+				sz.cy = bmpInfo.bmiHeader.biHeight;
+				c1 = sz.cx * sz.cy;
+	 
+				lpBits = (LPDWORD)::GlobalAlloc(GMEM_FIXED, (c1) * 4);
+	 
+				if (lpBits && ::GetDIBits(hdc, icInfo.hbmColor, 0, bmpInfo.bmiHeader.biHeight, lpBits, pBmpInfo, DIB_RGB_COLORS) != 0)
+				{
+					lpBitsPtr     = (LPBYTE)lpBits;
+					UINT off      = 0;
+	 
+					for (UINT i = 0; i < c1; i++)
+					{
+						off = (UINT)( 255 - (( lpBitsPtr[0] + lpBitsPtr[1] + lpBitsPtr[2] ) / 3) );
+	 
+						if (lpBitsPtr[3] != 0 || off != 255)
+						{
+							if (off == 0)
+							{
+								off = 1;
+							}
+	 
+							lpBits[i] = pPalette[off] | ( lpBitsPtr[3] << 24 );
+						}
+	 
+						lpBitsPtr += 4;
+					}
+	 
+					icGrayInfo.hbmColor = ::CreateCompatibleBitmap(hdc, sz.cx, sz.cy);
+	 
+					if (icGrayInfo.hbmColor != NULL)
+					{
+						::SetDIBits(hdc, icGrayInfo.hbmColor, 0, sz.cy, lpBits, &bmpInfo, DIB_RGB_COLORS);
+	 
+						icGrayInfo.hbmMask = icInfo.hbmMask;
+						icGrayInfo.fIcon   = TRUE;
+	 
+						hGrayIcon = ::CreateIconIndirect(&icGrayInfo);
+	 
+						::DeleteObject(icGrayInfo.hbmColor);
+					}
+	 
+					::GlobalFree(lpBits);
+					lpBits = NULL;
+				}
+			}
+	 
+			::DeleteObject(icInfo.hbmColor);
+			::DeleteObject(icInfo.hbmMask);
+		}
+	 
+		::ReleaseDC(NULL,hdc);
+	 
+		return hGrayIcon;
+	}
+	 
+	HICON CreateGrayscaleIcon( HICON hIcon )
+	{
+		if (hIcon == NULL)
+		{
+			return NULL;
+		}
+	 
+		COLORREF defaultGrayPalette[256];
+		BOOL bGrayPaletteSet = FALSE;
+		if (!bGrayPaletteSet)
+		{
+			for(int i = 0; i < 256; i++)
+			{
+				defaultGrayPalette[i] = RGB(255-i, 255-i, 255-i);
+			}
+	 
+			bGrayPaletteSet = TRUE;
+		}
+	 
+		return CreateGrayscaleIcon(hIcon, defaultGrayPalette);
+	}
+
+}
+
+bool CDynToolBar::AddButton(TBBUTTON& tbb, HICON hIcon)
+{
+	if (hIcon != 0)
+	{
+		// Add the icon to the imagelist's
+		if (!m_ImageList)
+		{
+			SetSizes( CSize( 20 + 7, 20 + 8 ), CSize( 20, 20 ) );
+
+			m_ImageList.Create(20, 20, ILC_COLOR32 | ILC_MASK, 0, 0);
+			GetToolBarCtrl().SetImageList(&m_ImageList);
+
+			m_DisabledImageList.Create(20, 20, ILC_COLOR32 | ILC_MASK, 0, 0);
+			GetToolBarCtrl().SetDisabledImageList(&m_DisabledImageList);
+		}
+
+		tbb.iBitmap = m_ImageList.Add( hIcon );
+		m_DisabledImageList.Add( CreateGrayscaleIcon(hIcon) );
+	}
+	else
+	{
+		tbb.iBitmap = I_IMAGENONE;
+	}
+
+    return GetToolBarCtrl().AddButtons( 1, &tbb )!=0;
+}
+
 /////////////////////////////////////////////////////////////////////////////
 // CDynToolBar message handlers
 #if defined(_WIN64)
@@ -46,7 +240,11 @@ int CDynToolBar::OnToolHitTest(CPoint point, TOOLINFO * pTI) const
 	ASSERT(::IsWindow(m_hWnd));
 	
 	// check child windows first by calling CControlBar
+#if defined(_WIN64)
+	INT_PTR nHit = (int)CControlBar::OnToolHitTest(point, pTI);
+#else
 	int nHit = (int)CControlBar::OnToolHitTest(point, pTI);
+#endif
 	if (nHit != -1)
 		return nHit;
 	
@@ -84,7 +282,7 @@ void CDynToolBar::OnToolBarGetButtonInfo(NMHDR* pNMHDR, LRESULT* pResult)
 	
 	if((pTBntf->iItem>=0) && (pTBntf->iItem < GetToolBarCtrl().GetButtonCount()))
 	{
-		TBBUTTON button;
+		TBBUTTON button = {0};
 		GetToolBarCtrl().GetButton(pTBntf->iItem, &button);
 		pTBntf->tbButton = button;
 		CString str;
@@ -116,7 +314,15 @@ void CDynToolBar::OnCustomize()
 
 void CDynToolBar::OnToolBarQueryDelete(NMHDR* pNMHDR, LRESULT* pResult)
 {
-	*pResult = TRUE;
+	TBNOTIFY* pTBntf = (TBNOTIFY *)pNMHDR;
+
+	// do not allow hidden button to be deleted as they just do not go
+	// to the Add listbox.
+	if ((pTBntf->tbButton.idCommand) && 
+		GetToolBarCtrl().IsButtonHidden(pTBntf->tbButton.idCommand))
+			*pResult = FALSE;
+	else					 
+			*pResult = TRUE;  
 }
 
 void CDynToolBar::OnToolBarQueryInsert(NMHDR* pNMHDR, LRESULT* pResult)
--- trunk/CDynToolbar/DynToolBar.h
+++ trunk/CDynToolbar/DynToolBar.h
@@ -7,6 +7,30 @@
 // DynToolBar.h : header file
 //
 
+// CRebar - Makes it possible to display Chevron
+// CRebar - Allowed one to convert "ugly" toolbars to flat toolbars (CToolbar now does that automatically)
+// CRebar - Allows one to put a bitmap behind the toolbars (Just like the old IE 3.0)
+// CRebar - Looks good on Win7/Vista
+
+// CFrameWnd::LoadBarState() / SaveBarState() handles position floating/undocked toolbars. http://support.microsoft.com/kb/232845
+//	- How do we handle this with CRebar ?
+// CToolBar::RestoreState() / SaveState() handles buttons hidden
+//	- This should work well with CRebar, but we still need to remember toolbar positions ?
+//		- Look at http://www.codeproject.com/KB/toolbars/toolbarex.aspx for how to customize with save and restore
+//	- Seems CDockState can load and save to the registry for us
+
+// Floating and docking of toolbars
+//	- CRebar doesn't support floating toolbars
+//	- CToolbar supports floating mode with help from CFrameWnd::EnableDocking()
+//		EnableDocking(0);
+//		m_wndToolBar.EnableDocking(0);
+//		FloatControlBar(&m_wndToolBar, CPoint(100,100));
+
+// CommandBar is a CToolbar with text-buttons. Need to create dropdown menu when clicking a text-button
+
+// CFrameWnd already contains something called CDockBar that are created when calling EnableDocking()
+//	- http://msdn.microsoft.com/en-us/library/ms997549.aspx
+
 /////////////////////////////////////////////////////////////////////////////
 // CDynToolBar window
 
@@ -21,6 +45,11 @@ public:
 
 // Operations
 public:
+	bool AddSeparator();
+	bool AddButton(int nIdCommand, HICON hIcon);
+	bool AddButton(int nIdCommand, LPCTSTR lpIconName);
+	bool AddButton(int nIdCommand, UINT lpIconName);
+	bool AddButton(TBBUTTON& tbb, HICON hIcon);
 
 // Overrides
 	// ClassWizard generated virtual function overrides
@@ -31,6 +60,8 @@ public:
 public:
 	virtual ~CDynToolBar();
 
+	//BOOL LoadToolBar(LPCTSTR lpszResourceName);
+	//BOOL LoadToolBar(UINT nIDResource);
 
 	// Generated message map functions
 protected:
@@ -53,6 +84,10 @@ protected:
 	afx_msg void OnToolBarChange(NMHDR* pNMHDR, LRESULT* pResult);
 	afx_msg void OnInitCustomize(NMHDR* pNMHDR, LRESULT* pResult);
 	//}}AFX_MSG
+
+private:
+	CImageList m_ImageList;
+	CImageList m_DisabledImageList;
 
 	DECLARE_MESSAGE_MAP()
 };
