f1cf7a08bdbd751c5dd63a325b39d083f3525a21
sweaty1 <sweaty1@6bb060f5-b3e4-d176-1c5f-5f5a4e21b952>
2011-06-07 21:16:52 +0000

    Made it possible to hide and show bands inside the CDynReBar using the context menu

--- trunk/CDynToolbar/DynReBar.cpp
+++ trunk/CDynToolbar/DynReBar.cpp
@@ -3,6 +3,54 @@
 
 #include "ViewConfigSection.h"
 
+BEGIN_MESSAGE_MAP(CDynReBar, CReBar)
+	//{{AFX_MSG_MAP(CDynReBar)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+	ON_WM_CONTEXTMENU()
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+void CDynReBar::OnContextMenu(CWnd* pWnd, CPoint point) 
+{
+	CMenu menu;
+	VERIFY( menu.CreatePopupMenu() );
+
+	TCHAR pText[256] = {0};
+	REBARBANDINFO rbbi = {0};
+	rbbi.cbSize = sizeof( rbbi );
+	rbbi.fMask  = RBBIM_TEXT | RBBIM_ID | RBBIM_STYLE;
+	rbbi.lpText = pText;
+	rbbi.cch = _countof(pText)-1;
+
+	int nCount = GetReBarCtrl().GetBandCount();
+	for ( int nBand = 0; nBand < nCount; nBand++ )
+	{
+		VERIFY( GetReBarCtrl().GetBandInfo( nBand, &rbbi ) );
+
+		UINT nFlags = MF_STRING;
+		if (rbbi.fStyle & RBBS_HIDDEN)
+			nFlags |= MF_UNCHECKED;
+		else
+			nFlags |= MF_CHECKED;
+		menu.AppendMenu(nFlags, nBand+1, rbbi.lpText);
+	}
+	int nResult = menu.TrackPopupMenu(TPM_LEFTALIGN | TPM_RETURNCMD,point.x,point.y,this);
+	if (nResult > 0)
+	{
+		int nBand = nResult-1;
+
+		VERIFY( GetReBarCtrl().GetBandInfo( nBand, &rbbi ) );
+		if (rbbi.fStyle & RBBS_HIDDEN)
+		{
+			ShowBand(nBand, TRUE);
+		}
+		else
+		{
+			ShowBand(nBand, FALSE);
+		}
+	}
+}
+
 BOOL CDynReBar::AddBar(CWnd* pBar, LPCTSTR pszText, CBitmap* pbmp, DWORD dwStyle)
 {
 	if (!CReBar::AddBar(pBar, pszText, pbmp, dwStyle))
@@ -31,6 +79,35 @@ BOOL CDynReBar::AddBar(CWnd* pBar, COLORREF clrFore, COLORREF clrBack, LPCTSTR p
 	return TRUE;
 }
 
+BOOL CDynReBar::ShowBand(int nBand, BOOL bShow)
+{
+	REBARBANDINFO rbbi = {0};
+	rbbi.cbSize = sizeof(REBARBANDINFO);
+	rbbi.fMask = RBBIM_STYLE | RBBIM_CHILD;
+	if (GetReBarCtrl().GetBandInfo(nBand, &rbbi)==FALSE)
+		return FALSE;
+
+	CWnd* pBandWnd = CWnd::FromHandlePermanent(rbbi.hwndChild);
+
+	if (bShow)
+	{
+		if (pBandWnd)
+			pBandWnd->ShowWindow(SW_SHOW);		
+
+		rbbi.fStyle &= ~RBBS_HIDDEN;
+
+	}
+	else
+	{
+		if (pBandWnd)
+			pBandWnd->ShowWindow(SW_HIDE);
+
+		rbbi.fStyle |= RBBS_HIDDEN;
+	}
+
+	return GetReBarCtrl().SetBandInfo(nBand, &rbbi);
+}
+
 CToolBar* CDynReBar::GetToolBar(int nBand)
 {
 	REBARBANDINFO rbbi = {0};
@@ -53,7 +130,7 @@ void CDynReBar::LoadState( CViewConfigSectionDefault& config )
 	{
 		REBARBANDINFO rbbi = {0};
 		rbbi.cbSize = sizeof( rbbi );
-		rbbi.fMask  = RBBIM_STYLE | RBBIM_SIZE | RBBIM_ID;
+		rbbi.fMask  = RBBIM_STYLE | RBBIM_SIZE | RBBIM_ID | RBBIM_CHILD;
 
 		int nCount = GetReBarCtrl().GetBandCount();
 		for ( int nBand = 0; nBand < nCount; nBand++ )
@@ -76,11 +153,10 @@ void CDynReBar::LoadState( CViewConfigSectionDefault& config )
 			rbbi.fStyle = ( rbbi.fStyle & ~( RBBS_HIDDEN | RBBS_BREAK ) ) | nBandStyle;
 			VERIFY( GetReBarCtrl().SetBandInfo( nBand, &rbbi ) );
 
-			CToolBar* pToolBar = GetToolBar(nBand);
-
-			if (pToolBar && (nBandStyle & RBBS_HIDDEN))
+			CWnd* pBandWnd = CWnd::FromHandlePermanent(rbbi.hwndChild);
+			if (pBandWnd && (nBandStyle & RBBS_HIDDEN))
 			{
-				pToolBar->ShowWindow(SW_HIDE);
+				pBandWnd->ShowWindow(SW_HIDE);
 			}
 		}
 	}
--- trunk/CDynToolbar/DynReBar.h
+++ trunk/CDynToolbar/DynReBar.h
@@ -11,7 +11,19 @@ public:
 	BOOL AddBar(CWnd* pBar, COLORREF clrFore, COLORREF clrBack,
 		LPCTSTR pszText = NULL, DWORD dwStyle = RBBS_GRIPPERALWAYS);
 
+	BOOL ShowBand(int nBand, BOOL bShow);
+
 	CToolBar* GetToolBar(int nBand);
 	void LoadState( CViewConfigSectionDefault& config );
 	void SaveState( CViewConfigSection& config );
+
+protected:
+	//{{AFX_MSG(CDynReBar)
+		// NOTE - the ClassWizard will add and remove member functions here.
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	//}}AFX_MSG
+
+private:
+
+	DECLARE_MESSAGE_MAP()
 };
